b72284391fba640750ace95a383fdea0263109ab
Layon José <layonjp300@gmail.com>
2024-05-22 10:31:47 -0300

    init

--- app.js
+++ app.js
@@ -1,51 +1,25 @@
-const express = require('express');
-const app = express();
-const axios = require("axios");
+const express = require('express'); // Importa o módulo Express
+const path = require('path'); // Importa o módulo 'path' para trabalhar com diretórios e caminhos de arquivos
+const app = express(); // Cria uma instância do Express
+const rotas = require('./routes/rotas'); // Importa as rotas definidas em 'routes/index.js'
 
-const PORT = 3000;
+// Configurações de view engine e múltiplas pastas de views
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'templates')]); // Define os diretórios de views
+app.set('view engine', 'ejs'); // Define 'ejs' como a view engine
+app.engine('html', require('ejs').renderFile); // Configura a view engine para renderizar arquivos HTML
 
-// Verificar se está rodando
-app.listen(PORT, function(){
-    console.log(`O express está rondando na porta ${PORT}`);
+// Middleware para servir arquivos estáticos da pasta 'public'
+app.use(express.static('public'));
+
+// Middleware para usar as rotas definidas
+app.use('/', rotas);
+
+// Middleware para tratar erro 404
+app.use((req, res, next) => {
+  res.status(404).render('404', { error: 'Página não encontrada' });
 });
 
-// Definindo view engine para ejs que vai renderizar as views da sua aplicação
-// EJS é uma engine de templates que permite a inclusão de código JavaScript dentro de templates HTML. 
-app.set("view engine", "ejs");
-
-// Configura o Express para servir arquivos estáticos a partir da pasta "public"
-app.use(express.static("public"));
-
-// Renderiza a página inicial
-app.get("/", (req, res) => {
-  res.render("index", { dog: null, error: null });
-});
-
-// Rota chamada quando o usuário aperta o botão e busca uma raça de cachorro
-app.get("/dog", async (req, res) => {    
-    const breed = req.query.breed.toLowerCase();   // Obtem a raça do cachorro digitada pelo usuário    
-    const APIUrl = `https://dog.ceo/api/breed/${breed}/images`;
-    let dog;
-    let error = null;
-
-    try{
-      const response = await axios.get(APIUrl); //faz uma requisição GET a API
-      dog = response.data;
-      //console.log(dog)
-      res.render("index", { dog, breed, error }); //envia os dados retornados pela API para a view
-    }catch (error) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-
-      if (error.response) {
-        if (error.response.status === 404) {
-          error = `A raça ${breed} não está listada`; 
-        }else{        
-          error = "Algo deu errado, tente novamente"; 
-        }       
-      }else{
-        error = "Erro na requisição. Tente novamente mais tarde."; 
-      }
-      res.render("index", {dog: null, error }); //envia o erro para o usuário na view
-    }
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
